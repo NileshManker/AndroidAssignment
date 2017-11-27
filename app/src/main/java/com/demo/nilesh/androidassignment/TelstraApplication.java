013c562b89e69a97c46bf19e57ece4a4631502e5
Nilesh Manker <nilesh.manker@team.telstra.com>
2017-11-27 10:57:12 +0530

    Added Application file.

---
+++ app/src/main/java/com/demo/nilesh/androidassignment/TelstraApplication.java
@@ -0,0 +1,15 @@
+package com.demo.nilesh.androidassignment;
+
+import android.app.Application;
+
+/**
+ * Created by nileshmanker on 27/11/17.
+ */
+
+public class TelstraApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
